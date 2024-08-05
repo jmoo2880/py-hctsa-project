2e71f46fed1e3ef8b91d03edd1ef5963a90c2353
jmoo2880 <jmoo2880@uni.sydney.edu.au>
2024-08-05 17:43:47 +1000

    Add length function

---
+++ Operations/ST_Length.py
@@ -0,0 +1,18 @@
+
+def ST_Length(y):
+    """
+    Length of an input data vector.
+
+    Parameters:
+    -----------
+    y : array_like
+        the time series to analyze.
+
+    Returns:
+    --------
+    out : float
+        the length of the time series
+    """
+    out = len(y)
+
+    return out
